0b81fe3dbbc19fd3df2663ad087dedf44214be88
gnlguys@gmail.com <gnlguys@gmail.com>
2021-08-11 16:36:12 +0300

    Caliburn.micro

---
+++ WpfAppExample/Model/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppExample.Model
+{
+    class Employee
+    {
+        public int TZ { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }  
+        public string Job { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool HasAddress { get; set; }
+
+        
+
+        public Employee()
+        {
+        }
+        public Employee(int tz, string firstName, string lastName, string job, DateTime startDate, bool hasAddress)
+        { 
+            this.TZ = tz;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Job = job;
+            this.StartDate = startDate;
+            this.HasAddress = hasAddress;
+
+        }
+    }
+}
---
+++ WpfAppExample/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppExample.ViewModels
+{
+    class ShellViewModel : Screen
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+
+    }
+}
